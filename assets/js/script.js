f8aff0fbb7d4f73bf22e655dfac616362211fee2
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-27 22:34:01 -0500

    add switch case for start and end page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,8 @@ var question3 = document.getElementById("question3")
 var question4 = document.getElementById("question4")
 var question5 = document.getElementById("question5")
 var question6 = document.getElementById("question6")
+var startPage = document.getElementById("begin")
+var scoreSubmission = document.getElementById("scoresubmission")
 
 // Listens for clicks on an answer and judges how to proceed based on correct/incorrect and what question we're on
 document.body.addEventListener ("click", function (event) {
@@ -17,6 +19,8 @@ document.body.addEventListener ("click", function (event) {
         switch (event.target.id) {
             case "begin":
             console.log("begin");
+            startPage.setAttribute("style", "display:none")
+            question1.setAttribute("style", "display:block")
             break;
             case "correct1":
             console.log ("correct!");
@@ -24,51 +28,62 @@ document.body.addEventListener ("click", function (event) {
             question2.setAttribute("style", "display:block")
             break;
             case "correct2":
-            question1.setAttribute("style", "display:none")
-            question2.setAttribute("style", "display:block")
+            question2.setAttribute("style", "display:none")
+            question3.setAttribute("style", "display:block")
             console.log ("correct!");
             break;
             case "correct3":
-            question1.setAttribute("style", "display:none")
-            question2.setAttribute("style", "display:block")
+            question3.setAttribute("style", "display:none")
+            question4.setAttribute("style", "display:block")
             console.log ("correct!");
             break;
             case "correct4":
-            question1.setAttribute("style", "display:none")
-            question2.setAttribute("style", "display:block")
+            question4.setAttribute("style", "display:none")
+            question5.setAttribute("style", "display:block")
             console.log ("correct!");
             break;
             case "correct5":
-            question1.setAttribute("style", "display:none")
-            question2.setAttribute("style", "display:block")
+            question5.setAttribute("style", "display:none")
+            question6.setAttribute("style", "display:block")
             console.log ("correct!");
             break;
             case "correct6":
-            question1.setAttribute("style", "display:none")
-            question2.setAttribute("style", "display:block")
+            question6.setAttribute("style", "display:none")
+            scoreSubmission.setAttribute("style", "display:block")
             console.log ("correct!");
             break;
         }
         switch (event.target.className){
             case "wrong1":
             console.log ("incorrect!");
+            question1.setAttribute("style", "display:none")
+            question2.setAttribute("style", "display:block")
             break;
             case "wrong2":
             console.log ("incorrect!");
+            question2.setAttribute("style", "display:none")
+            question3.setAttribute("style", "display:block")
             break;
             case "wrong3":
             console.log ("incorrect!");
+            question3.setAttribute("style", "display:none")
+            question4.setAttribute("style", "display:block")
             break;
             case "wrong4":
             console.log ("incorrect!");
+            question4.setAttribute("style", "display:none")
+            question5.setAttribute("style", "display:block")
             break;
             case "wrong5":
             console.log ("incorrect!");
+            question5.setAttribute("style", "display:none")
+            question6.setAttribute("style", "display:block")
             break;
             case "wrong6":
             console.log ("incorrect!");
+            question6.setAttribute("style", "display:none")
+            scoreSubmission.setAttribute("style", "display:block")
             break;
         }
     }
 })
-
