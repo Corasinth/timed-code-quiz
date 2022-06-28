7ede7e04e7d1b86b7d4432dc056a409075440e79
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-28 00:13:35 -0500

    consolidate all switch cases into a single for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,42 @@
 
 
 
-//Variables
+//=====================Variables=====================
+
+//Identifying 'pages'
+var startPage = document.getElementById("startpage")
 var question1 = document.getElementById("question1")
 var question2 = document.getElementById("question2")
 var question3 = document.getElementById("question3")
 var question4 = document.getElementById("question4")
 var question5 = document.getElementById("question5")
 var question6 = document.getElementById("question6")
-var startPage = document.getElementById("begin")
 var scoreSubmission = document.getElementById("scoresubmission")
+
+var pageArray = [startPage, question1, question2, question3, question4, question5, question6,scoreSubmission]
 
 // Listens for clicks on an answer and judges how to proceed based on correct/incorrect and what question we're on
 document.body.addEventListener ("click", function (event) {
     if (event.target.matches("button") === true) {
-        switch (event.target.id) {
+       for (var i = 0; i<pageArray.length; i++) {
+        var currentIndex = pageArray[i]
+        if (window.getComputedStyle((currentIndex)).display === "block") {
+            pageArray[i].setAttribute("style", "display:none")
+            if ([i] == pageArray.length - 1) {
+            pageArray[0].setAttribute("style", "display:block");
+            break;
+        } else {
+            console.log (pageArray[i+1])
+            pageArray[i+1].setAttribute("style", "display:block");
+            break;
+        }
+        }
+        }
+    }
+})
+
+
+/* switch (event.target.id) {
             case "begin":
             console.log("begin");
             startPage.setAttribute("style", "display:none")
@@ -88,5 +110,4 @@ document.body.addEventListener ("click", function (event) {
             scoreSubmission.setAttribute("style", "display:block")
             break;
         }
-    }
-})
+        */
