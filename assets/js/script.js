98b09442b0d47abf0e82b3d5f5453734b8fffd62
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-27 22:20:57 -0500

    assign variable to each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+
+
+
+
+
+//Variables
+var question1 = document.getElementById("question1")
+var question2 = document.getElementById("question2")
+var question3 = document.getElementById("question3")
+var question4 = document.getElementById("question4")
+var question5 = document.getElementById("question5")
+var question6 = document.getElementById("question6")
+
 // Listens for clicks on an answer and judges how to proceed based on correct/incorrect and what question we're on
 document.body.addEventListener ("click", function (event) {
     if (event.target.matches("button") === true) {
@@ -46,3 +59,4 @@ document.body.addEventListener ("click", function (event) {
         }
     }
 })
+
