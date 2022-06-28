20573935ea64b0fecaada8a910431eb917dde871
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-28 02:19:12 -0500

    change scorekeeping to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,12 @@ var numberOfCorrect= 0
 
 //Identifying timer fields
 var seconds = document.getElementById("secondCounter")
-var timeLeft = 100
+var timeLeft = 60
 var setTime
 
 //Identifying highscore fields
 var initialsInput = document.querySelector("#initials");
+var scoreArray = []
 
 //=====================Functions=====================
 
@@ -56,7 +57,7 @@ seconds.textContent = timeLeft
 
 //Calculates score so that a person who runs out of time but got correct answers doesn't get a score of 0
 function calculateScore (timeScore, correctAnswers) {
-    score = timeScore + (correctAnswers*17);
+    score = timeScore + (correctAnswers*10);
     return score;
 }
 
@@ -71,7 +72,7 @@ document.body.addEventListener ("click", function (event) {
     }
     //Create time penalty for incorrect answer
     if (event.target.matches(".wrong") === true) {
-        timeLeft -= 17
+        timeLeft -= 10
         displayTime();
     //Keeps track of correct answers for score calculator
     } else if (event.target.matches(".correct") === true) {
@@ -79,20 +80,20 @@ document.body.addEventListener ("click", function (event) {
     }
     //Handles form submission and resetting the quiz
     if (event.target.id === "submit") {
-        displayTime()
         calculateScore(timeLeft, numberOfCorrect)
-        localStorage.setItem("initials", initialsInput.value)
-        localStorage.setItem("score", score)
-        console.log (score)
-        timeLeft = 100
-        score = 0
+        scoreArray.push (initialsInput.value + " | " + score)
+        console.log (scoreArray)
+        localStorage.setItem("scoreArray", JSON.stringify(scoreArray))
+        timeLeft = 60
+        score = 0       
+        displayTime()
     }
     //Handles resetting quiz without submitting form
     if (event.target.id === "submit1") {
-        displayTime()
         calculateScore(timeLeft, numberOfCorrect)
-        timeLeft = 100
+        timeLeft = 60
         score = 0
+        displayTime()
     }   
  //Handles making the correct set of elements appear and dissapear
     if (event.target.matches("button") === true) {
